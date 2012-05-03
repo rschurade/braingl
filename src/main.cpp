6c480fae58f68d8e1b1d3c52da1db9ebef2de6bd
schurade <schurade@localhost>
2012-05-03 13:00:38 +0200

    setup example project with all neccessary build tools

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
-// dummy
+#include <QtGui/QApplication>
+
+#include "gui/mainwindow.h"
+
+int main(int argc, char *argv[]) {
+	QApplication app(argc, argv);
+	Q_INIT_RESOURCE(icons);
+	MainWindow mainWin;
+	mainWin.show();
+	return app.exec();
+}
