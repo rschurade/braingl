2026d3a9b911efd53776cfe27637d7b1715a3480
schurade <schurade@localhost>
2012-05-18 14:35:03 +0200

    multitexturing for 5 scalar textures, grey values

--- src/shaders/fshader.glsl
+++ src/shaders/fshader.glsl
@@ -34,15 +34,54 @@ varying vec3 v_texcoord;
 
 void main()
 {
-//	vec4 color;
-//	color = vec4( 0.0, 0.0, 0.0, 1.0 );
+	vec4 color;
+	color = vec4( 0.0, 0.0, 0.0, 1.0 );
+
 	if ( u_texActive0 )
 	{
-		if( texture3D( texture0, v_texcoord ).r < u_lowerThreshold0 )
-			discard;
-		if( texture3D( texture0, v_texcoord ).r > u_upperThreshold0 )
-			discard;
-		gl_FragColor = texture3D( texture0, v_texcoord );
+		vec4 color0 = texture3D( texture0, v_texcoord );
+		if( color0.r > u_lowerThreshold0 && color0.r <= u_upperThreshold0 )
+		{
+			color = color0;
+		}
 	}
-	//gl_FragColor = color;
+
+	if ( u_texActive1 )
+	{
+		vec4 color1 = texture3D( texture1, v_texcoord );
+		if( color1.r > u_lowerThreshold1 && color1.r <= u_upperThreshold1 )
+		{
+			color.rgb =  mix( color.rgb, color1.rgb, u_alpha1 );
+		}
+	}
+	
+	if ( u_texActive2 )
+	{
+		vec4 color2 = texture3D( texture2, v_texcoord );
+		if( color2.r > u_lowerThreshold2 && color2.r <= u_upperThreshold2 )
+		{
+			color.rgb =  mix( color.rgb, color2.rgb, u_alpha2 );
+		}
+	}
+	
+	if ( u_texActive3 )
+	{
+		vec4 color3 = texture3D( texture3, v_texcoord );
+		if( color3.r > u_lowerThreshold3 && color3.r <= u_upperThreshold3 )
+		{
+			color.rgb =  mix( color.rgb, color3.rgb, u_alpha3 );
+		}
+	}
+	
+	if ( u_texActive4 )
+	{
+		vec4 color4 = texture3D( texture4, v_texcoord );
+		if( color4.r > u_lowerThreshold4 && color4.r <= u_upperThreshold4 )
+		{
+			color.rgb =  mix( color.rgb, color4.rgb, u_alpha4 );
+		}
+	}
+
+	if ( color.r < 0.00001 ) discard; 
+	gl_FragColor = color;
 }
