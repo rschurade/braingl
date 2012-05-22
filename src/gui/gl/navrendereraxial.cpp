d9ad6910291fcd4df39f3e0eda0aa50e4edc187a
schurade <rschurade@users.noreply.github.com>
2012-05-22 14:28:27 +0200

    centered nav slices

--- src/gui/gl/navrendereraxial.cpp
+++ src/gui/gl/navrendereraxial.cpp
@@ -26,7 +26,6 @@ NavRendererAxial::~NavRendererAxial()
 void NavRendererAxial::adjustRatios()
 {
     m_ratio = static_cast< float >( m_width ) / static_cast< float >( m_height );
-    glViewport( 0, 0, m_width, m_height );
 
     // Reset projection
     QMatrix4x4 pMatrix;
@@ -37,11 +36,17 @@ void NavRendererAxial::adjustRatios()
 
     if ( mult >= 1.0 )
     {
-        pMatrix.ortho( 0, m_xb * mult, 0, m_yb, -3000, 3000 );
+        pMatrix.ortho( 0, m_xb, 0, m_yb, -3000, 3000 );
+
+        int xoff = ( m_width - ( m_width / mult ) ) / 2;
+        glViewport( xoff, 0, m_width - ( 2* xoff) , m_height );
     }
     else
     {
-        pMatrix.ortho( 0, m_xb, 0, m_yb * ( 1.0 / mult ), -3000, 3000 );
+        pMatrix.ortho( 0, m_xb, 0, m_yb, -3000, 3000 );
+
+        int yoff = ( m_height - ( m_height * mult ) ) / 2;
+        glViewport( 0, yoff, m_width, m_height - ( 2 * yoff ) );
     }
     m_mvpMatrix = pMatrix;
 }
