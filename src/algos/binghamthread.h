68af96469483fc72eeb3135738acadba90742644
schurade <schurade@localhost>
2013-01-11 10:57:50 +0100

    bingham fit parallel

---
+++ src/algos/binghamthread.h
@@ -0,0 +1,47 @@
+/*
+ * bingham.h
+ *
+ *  Created on: 27.12.2012
+ *      Author: Ralph
+ */
+
+#ifndef BINGHAMTHREAD_H_
+#define BINGHAMTHREAD_H_
+
+#include <QtCore/QDebug>
+#include <QtCore/QThread>
+#include <QtCore/QVector>
+
+#include "../thirdparty/newmat10/newmat.h"
+#include "../thirdparty/newmat10/newmatap.h"
+
+class DatasetSH;
+
+class BinghamThread : public QThread
+{
+public:
+    BinghamThread( DatasetSH* ds, int lod, int id );
+    virtual ~BinghamThread();
+
+    QVector<QVector<float> >* getResultVector();
+
+private:
+    void run();
+
+    QVector<float> fit_bingham( const ColumnVector& sh_data,
+                                  const Matrix& tess,
+                                  const QVector<QSet<int> >& adj,
+                                  const Matrix& base,
+                                  const int neighborhood,
+                                  const int num_max );
+
+
+    DatasetSH* m_ds;
+    int m_lod;
+
+    int m_id;
+
+    QVector<QVector<float> >* m_resultVector;
+};
+
+#endif /* BINGHAMTHREAD_H_ */
