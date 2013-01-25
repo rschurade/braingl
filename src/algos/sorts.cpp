230bac24baa68832a55163990a8da09df8d95720
schurade <schurade@localhost>
2013-01-25 15:47:54 +0100

    refactor: pointer to reference

--- src/algos/sorts.cpp
+++ src/algos/sorts.cpp
@@ -10,16 +10,13 @@
 
 Sorts::Sorts()
 {
-    // TODO Auto-generated constructor stub
-
 }
 
 Sorts::~Sorts()
 {
-    // TODO Auto-generated destructor stub
 }
 
-void Sorts::quickFindFirstK( QVector<int>* ids, QVector<float>* vals, int left, int right, int k )
+void Sorts::quickFindFirstK( QVector<int>& ids, QVector<float>& vals, int left, int right, int k )
 {
     //qDebug() << "qffk: " << left << right;
     if ( right > left )
@@ -35,16 +32,16 @@ void Sorts::quickFindFirstK( QVector<int>* ids, QVector<float>* vals, int left, 
     }
 }
 
-int Sorts::partition( QVector<int>* ids, QVector<float>* vals, int left, int right, int pivotIndex )
+int Sorts::partition( QVector<int>& ids, QVector<float>& vals, int left, int right, int pivotIndex )
 {
-    int pivotValue = vals->at( ids->at( pivotIndex ) );
+    int pivotValue = vals.at( ids.at( pivotIndex ) );
 
     Sorts::swap ( ids, pivotIndex, right );  // Move pivot to end
     int storeIndex = left;
 
     for ( int i = left; i <= right; ++i )  //      for i from left to right
     {
-        if ( vals->at( ids->at (i ) ) > pivotValue )
+        if ( vals.at( ids.at (i ) ) > pivotValue )
         {
             Sorts::swap ( ids, storeIndex, i );
              ++storeIndex;
@@ -54,31 +51,31 @@ int Sorts::partition( QVector<int>* ids, QVector<float>* vals, int left, int rig
     return storeIndex;
 }
 
-void Sorts::swap( QVector<int>* ids, int id1, int id2 )
+void Sorts::swap( QVector<int>& ids, int id1, int id2 )
 {
-    int tmp = ids->at( id1 );
-    ids->replace( id1, ids->at( id2 ) );
-    ids->replace( id2, tmp );
+    int tmp = ids.at( id1 );
+    ids.replace( id1, ids.at( id2 ) );
+    ids.replace( id2, tmp );
 }
 
-void Sorts::quickSort( QVector<int>* ids, QVector<float>* vals, int left, int right )
+void Sorts::quickSort( QVector<int>& ids, QVector<float>& vals, int left, int right )
 {
     int i = left, j = right;
     int tmp;
-    float pivot = vals->at( ids->at( left + ( right - left ) / 2 ) );
+    float pivot = vals.at( ids.at( left + ( right - left ) / 2 ) );
 
     /* partition */
     while ( i <= j )
     {
-        while ( vals->at( ids->at( i ) ) > pivot )
+        while ( vals.at( ids.at( i ) ) > pivot )
             ++i;
-        while ( vals->at( ids->at( j ) ) < pivot )
+        while ( vals.at( ids.at( j ) ) < pivot )
             --j;
         if ( i <= j )
         {
-            tmp = ids->at( i );
-            ids->replace(  i, ids->at( j ) );
-            ids->replace(  j, tmp );
+            tmp = ids.at( i );
+            ids.replace(  i, ids.at( j ) );
+            ids.replace(  j, tmp );
             ++i;
             --j;
         }
