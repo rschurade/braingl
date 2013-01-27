93881980228b5f6b6493cd21edd3c39080a3761c
schurade <rschurade@users.noreply.github.com>
2013-01-27 14:27:23 +0100

    added mouse interaction to combined nav renderer

--- src/gui/gl/combinednavrenderer.h
+++ src/gui/gl/combinednavrenderer.h
@@ -49,12 +49,15 @@ private:
 
     GLuint *vboIds;
 
-    float m_x;
-    float m_y;
-    float m_z;
-    float m_xb;
-    float m_yb;
-    float m_zb;
+    int m_x;
+    int m_y;
+    int m_z;
+    float m_dx;
+    float m_dy;
+    float m_dz;
+    int m_nx;
+    int m_ny;
+    int m_nz;
 
     int m_width;
     int m_height;
