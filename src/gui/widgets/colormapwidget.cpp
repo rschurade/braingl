a3587d47515f7e9994dbb6fc0dc560fa6210c3b9
schurade <rschurade@users.noreply.github.com>
2013-01-29 17:14:36 +0100

    added a colormap widget to dataset properties

---
+++ src/gui/widgets/colormapwidget.cpp
@@ -0,0 +1,120 @@
+/*
+ * colormapwidget.cpp
+ *
+ *  Created on: Jan 29, 2013
+ *      Author: schurade
+ */
+#include <QtGui/QtGui>
+
+#include "colormapwidget.h"
+
+ColormapWidget::ColormapWidget( int width ) :
+    m_width( width ),
+    m_lowerThreshold( 0.0 ),
+    m_upperThreshold( 1.0 )
+{
+    QVBoxLayout* vLayout = new QVBoxLayout();
+
+//    QHBoxLayout* hLayout = new QHBoxLayout();
+//    m_nlabel = new QLabel( QString("colormap"), this );
+//    hLayout->addWidget( m_nlabel );
+//    hLayout->addStretch();
+
+    QHBoxLayout* hLayout2 = new QHBoxLayout();
+    m_nlabel = new QLabel( this );
+
+    m_image = createImage( width );
+    QPixmap pix( width, 20 );
+    pix.convertFromImage( *m_image );
+    m_nlabel->setPixmap( pix );
+    hLayout2->addWidget( m_nlabel );
+
+    //vLayout->addLayout( hLayout );
+    vLayout->addLayout( hLayout2 );
+    vLayout->setContentsMargins( 1,1,1,1 );
+
+    setLayout( vLayout );
+
+    setFrameStyle( QFrame::Panel | QFrame::Raised );
+
+    setStyleSheet( "QLabel { font:  bold 12px; margin-bottom: -1px }"
+                     "QLineEdit { font: 12px; max-height: 14px; margin-top: -1px }"
+                   );
+}
+
+ColormapWidget::~ColormapWidget()
+{
+}
+
+QImage* ColormapWidget::createImage( int width )
+{
+    QImage* image = new QImage( width, 20, QImage::Format_RGB32 );
+
+    for ( int i = 0; i < width; ++i )
+    {
+        QColor c = colormap( (float)i / (float)width );
+        for ( int k = 0; k < 20; ++k )
+        {
+            image->setPixel( i, k, c.rgba() );
+        }
+    }
+
+    return image;
+}
+
+QColor ColormapWidget::colormap( float value )
+{
+    value = ( value - m_lowerThreshold ) / ( m_upperThreshold - m_lowerThreshold );
+
+    value = qMax( 0.0f, qMin( 1.0f, value ) );
+    value *= 5.0;
+    float r = 0.0;
+    float g = 0.0;
+    float b = 0.0;
+    if( value < 1.0 )
+    {
+        g = value;
+        b = 1.0f;
+    }
+    else if( value < 2.0 )
+    {
+        g = 1.0;
+        b = 2.0 -value;
+    }
+    else if( value < 3.0 )
+    {
+        r = value-2.0;
+        g = 1.0;
+    }
+    else if( value < 4.0 )
+    {
+        r = 1.0;
+        g = 4.0-value;
+    }
+    else
+    {
+        r = 1.0;
+        b = value-4.0;
+    }
+    return QColor( 255 * r, 255 * g, 255 * b );
+}
+
+void ColormapWidget::setLowerThreshold( float value )
+{
+    value = qMax( 0.0f, qMin( 1.0f, value ) );
+    m_lowerThreshold = value;
+    m_image = createImage( m_width );
+    QPixmap pix( m_width, 20 );
+    pix.convertFromImage( *m_image );
+    m_nlabel->setPixmap( pix );
+}
+
+void ColormapWidget::setUpperThreshold( float value )
+{
+    value = qMax( 0.0f, qMin( 1.0f, value ) );
+    m_upperThreshold = value;
+    m_image = createImage( m_width );
+    QPixmap pix( m_width, 20 );
+    pix.convertFromImage( *m_image );
+    m_nlabel->setPixmap( pix );
+}
---
+++ src/gui/widgets/colormapwidget.h
@@ -0,0 +1,41 @@
+/*
+ * colormapwidget.h
+ *
+ *  Created on: Jan 29, 2013
+ *      Author: schurade
+ */
+
+#ifndef COLORMAPWIDGET_H_
+#define COLORMAPWIDGET_H_
+
+#include <QtGui/QFrame>
+
+class QImage;
+class QLabel;
+
+class ColormapWidget : public QFrame
+{
+    Q_OBJECT
+
+public:
+    ColormapWidget( int width );
+    virtual ~ColormapWidget();
+
+private:
+    QImage* createImage( int width );
+    QColor colormap( float value );
+
+    QImage* m_image;
+    QLabel* m_nlabel;
+    QLabel* m_clabel;
+
+    int m_width;
+    float m_lowerThreshold;
+    float m_upperThreshold;
+
+public slots:
+    void setLowerThreshold( float value );
+    void setUpperThreshold( float value );
+};
+
+#endif /* COLORMAPWIDGET_H_ */
