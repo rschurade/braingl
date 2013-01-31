aea8670c43a26b626f5dcefac9fde402bb88cd38
schurade <rschurade@users.noreply.github.com>
2013-01-31 12:16:08 +0100

    select the last loaded dataset

--- src/gui/views/datasetlistview.cpp
+++ src/gui/views/datasetlistview.cpp
@@ -4,6 +4,8 @@
  *  Created on: 08.05.2012
  *      Author: Ralph
  */
+#include <QtCore/QDebug>
+
 #include "datasetlistview.h"
 
 DatasetListView::DatasetListView( QWidget *parent ) : QListView( parent )
@@ -17,4 +19,11 @@ DatasetListView::~DatasetListView()
 void DatasetListView::selectionChanged( const QItemSelection &selected, const QItemSelection &deselected )
 {
 	emit itemSelectionChanged( selected );
+}
+
+void DatasetListView::rowsInserted (const QModelIndex &parent, int start, int end )
+{
+    qDebug() << "inserted" << parent << start << end;
+    selectionModel()->clear();
+    selectionModel()->select( model()->index( start, 0, parent ), QItemSelectionModel::Select );
 }
--- src/gui/views/datasetlistview.h
+++ src/gui/views/datasetlistview.h
@@ -20,6 +20,7 @@ public:
 
 private slots:
 	void selectionChanged( const QItemSelection &selected, const QItemSelection &deselected );
+	void rowsInserted (const QModelIndex &parent, int start, int end );
 
 signals:
 	void itemSelectionChanged( const QItemSelection &selected );
