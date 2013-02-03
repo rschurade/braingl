871138dc136aa0d6b5f04c559d8317a2220ba74e
schurade <rschurade@users.noreply.github.com>
2013-02-03 20:33:19 +0100

    pass globals to rois

--- src/data/selectionbox.cpp
+++ src/data/selectionbox.cpp
@@ -4,19 +4,29 @@
  *  Created on: 02.02.2013
  *      Author: Ralph
  */
-
 #include "selectionbox.h"
 
-SelectionBox::SelectionBox( QString name, int x, int y, int z, float dx, float dy, float dz, bool neg ) :
-    ROI( name )
+#include <QtCore/QAbstractItemModel>
+
+SelectionBox::SelectionBox( QString name, QAbstractItemModel* globals ) :
+    ROI( name, globals )
 {
-    m_properties.set( Fn::ROI::X, x, true );
-    m_properties.set( Fn::ROI::Y, y, true );
-    m_properties.set( Fn::ROI::Z, z, true );
-    m_properties.set( Fn::ROI::DX, dx, true );
-    m_properties.set( Fn::ROI::DY, dy, true );
-    m_properties.set( Fn::ROI::DZ, dz, true );
-    m_properties.set( Fn::ROI::NEG, neg, true );
+    int x = globals->data( globals->index( (int)Fn::Global::SAGITTAL, 0 ) ).toInt();
+    int y = globals->data( globals->index( (int)Fn::Global::CORONAL, 0 ) ).toInt();
+    int z = globals->data( globals->index( (int)Fn::Global::AXIAL, 0 ) ).toInt();
+
+    int xMax = globals->data( globals->index( (int)Fn::Global::MAX_SAGITTAL, 0 ) ).toInt();
+    int yMax = globals->data( globals->index( (int)Fn::Global::MAX_CORONAL, 0 ) ).toInt();
+    int zMax = globals->data( globals->index( (int)Fn::Global::MAX_AXIAL, 0 ) ).toInt();
+
+
+    m_properties.set( Fn::ROI::X, x, 0, xMax, true );
+    m_properties.set( Fn::ROI::Y, y, 0, yMax, true );
+    m_properties.set( Fn::ROI::Z, z, 0, zMax, true );
+    m_properties.set( Fn::ROI::DX, 5, 0, xMax/2, true );
+    m_properties.set( Fn::ROI::DY, 5, 0, yMax/2, true );
+    m_properties.set( Fn::ROI::DZ, 5, 0, zMax/2, true );
+    m_properties.set( Fn::ROI::NEG, false, true );
     m_properties.set( Fn::ROI::TYPE, (int)Fn::ROIType::Box );
 }
 
