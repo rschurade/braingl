974011e59dae875e41f1aa9c20f5c841af97713b
schurade <schurade@localhost>
2013-02-07 09:34:23 +0100

    added static vairable pickIndex to glFunctions removed some pointer cast warnings

--- src/gui/gl/navrendereraxial.cpp
+++ src/gui/gl/navrendereraxial.cpp
@@ -178,11 +178,11 @@ void NavRendererAxial::draw()
         // Tell OpenGL which VBOs to use
         glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, vboIds[ 3 ] );
         glBindBuffer( GL_ARRAY_BUFFER, vboIds[ 2 ] );
-        long int offset = 0;
+
         // Tell OpenGL programmable pipeline how to locate vertex position data
         int vertexLocation = GLFunctions::getShader( "crosshair" )->attributeLocation( "a_position" );
         GLFunctions::getShader( "crosshair" )->enableAttributeArray( vertexLocation );
-        glVertexAttribPointer( vertexLocation, 3, GL_FLOAT, GL_FALSE, sizeof( VertexData ), (const void *) offset );
+        glVertexAttribPointer( vertexLocation, 3, GL_FLOAT, GL_FALSE, sizeof( VertexData ), 0 );
         // Draw cube geometry using indices from VBO 0
         glDrawElements( GL_LINES, 4, GL_UNSIGNED_SHORT, 0 );
         glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, 0 );
