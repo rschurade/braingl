ab7d6f0a45800dfce5c42e881c2fc7abb9caca2f
schurade <schurade@localhost>
2013-04-08 09:18:40 +0200

    for fiber data set slider values to mins and max

--- src/data/datasets/datasetfibers.cpp
+++ src/data/datasets/datasetfibers.cpp
@@ -216,5 +216,7 @@ void DatasetFibers::dataModeChanged()
     static_cast<PropertyFloat*> ( m_properties.getProperty( Fn::Property::SELECTED_MIN ) )->setMax( max );
     static_cast<PropertyFloat*> ( m_properties.getProperty( Fn::Property::SELECTED_MAX ) )->setMin( min );
     static_cast<PropertyFloat*> ( m_properties.getProperty( Fn::Property::SELECTED_MAX ) )->setMax( max );
+    static_cast<PropertyFloat*> ( m_properties.getProperty( Fn::Property::SELECTED_MIN ) )->setValue( min );
+    static_cast<PropertyFloat*> ( m_properties.getProperty( Fn::Property::SELECTED_MAX ) )->setValue( max );
     Models::d()->submit();
 }
