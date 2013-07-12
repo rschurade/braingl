6bfc73474633fe998719d0d9035a79f910b0226d
joachim <joachim@visualistics.de>
2013-07-12 20:03:17 +0200

    braingl as network server

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,6 @@
 #include "data/vptr.h"
 #include "gui/mainwindow.h"
 
-
-
 QTextStream *out = 0;
 bool logToFile = false;
 bool verbose = false;
@@ -19,41 +17,42 @@ void logOutput( QtMsgType type, const char *msg )
     QString filedate = QDateTime::currentDateTime().toString( "yyyy.MM.dd hh:mm:ss:zzz" );
     QString debugdate = QDateTime::currentDateTime().toString( "hh:mm:ss:zzz" );
 
-    switch (type)
+    switch ( type )
     {
-    case QtDebugMsg:
-        debugdate += " [D]";
-        break;
-    case QtWarningMsg:
-        debugdate += " [W]";
-        break;
-    case QtCriticalMsg:
-        debugdate += " [C]";
-        break;
-    case QtFatalMsg:
-        debugdate += " [F]";
-        break;
+        case QtDebugMsg:
+            debugdate += " [D]";
+            break;
+        case QtWarningMsg:
+            debugdate += " [W]";
+            break;
+        case QtCriticalMsg:
+            debugdate += " [C]";
+            break;
+        case QtFatalMsg:
+            debugdate += " [F]";
+            break;
     }
     if ( verbose )
     {
-        (*out) << debugdate << " " << msg << endl;
+        ( *out ) << debugdate << " " << msg << endl;
     }
     if ( logToFile )
     {
-        QFile outFile("log.txt");
-        outFile.open(QIODevice::WriteOnly | QIODevice::Append);
-        QTextStream ts(&outFile);
+        QFile outFile( "log.txt" );
+        outFile.open( QIODevice::WriteOnly | QIODevice::Append );
+        QTextStream ts( &outFile );
         ts << filedate << " " << msg << endl;
     }
 
-    if (QtFatalMsg == type)
+    if ( QtFatalMsg == type )
     {
         abort();
     }
 }
 
-void noOutput(QtMsgType type, const char *msg) {}
-
+void noOutput( QtMsgType type, const char *msg )
+{
+}
 
 int main( int argc, char *argv[] )
 {
@@ -61,13 +60,12 @@ int main( int argc, char *argv[] )
     qDebug() << "(c) 2012, 2013 Ralph Schurade, Joachim Boettger";
     qDebug() << "Submit suggestions, feature requests, bug reports to https://code.google.com/p/braingl/";
 
-
     QApplication app( argc, argv );
 
     QCoreApplication::setOrganizationDomain( "braingl.de" );
-	QCoreApplication::setOrganizationName( "MPI_CBS" );
-	QCoreApplication::setApplicationName( "braingl" );
-	QCoreApplication::setApplicationVersion( "1.0.0" );
+    QCoreApplication::setOrganizationName( "MPI_CBS" );
+    QCoreApplication::setApplicationName( "braingl" );
+    QCoreApplication::setApplicationVersion( "1.0.0" );
 
     Q_INIT_RESOURCE( resources );
 
