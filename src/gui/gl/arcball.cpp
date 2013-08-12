f05dcdf57379a1aba7bc5dfcd3811e35c5b3a20d
schurade <schurade@localhost>
2013-08-12 19:50:31 +0200

    added arcball to scripting

--- src/gui/gl/arcball.cpp
+++ src/gui/gl/arcball.cpp
@@ -230,3 +230,89 @@ void ArcBall::setState( QList<QVariant> state )
     m_rotCenter = state[9].value<QVector3D>();
     q_current_rotation = state[10].value<QQuaternion>();
 }
+
+QQuaternion ArcBall::getRotation()
+{
+    return mat2quat( m_currentRot );
+}
+
+void ArcBall::setRotation( QQuaternion quat )
+{
+    m_currentRot.setToIdentity();
+    m_lastRot.setToIdentity();
+    m_currentRot.rotate( quat );
+}
+
+inline float SIGN(float x) {return (x >= 0.0f) ? +1.0f : -1.0f;}
+inline float NORM(float a, float b, float c, float d) {return sqrt(a * a + b * b + c * c + d * d);}
+
+QQuaternion ArcBall::mat2quat( QMatrix4x4 &mat )
+{
+    double r11 = mat( 0, 0 );
+    double r12 = mat( 0, 1 );
+    double r13 = mat( 0, 2 );
+    double r21 = mat( 1, 0 );
+    double r22 = mat( 1, 1 );
+    double r23 = mat( 1, 2 );
+    double r31 = mat( 2, 0 );
+    double r32 = mat( 2, 1 );
+    double r33 = mat( 2, 2 );
+
+    double q0 = ( r11 + r22 + r33 + 1.0f ) / 4.0f;
+    double q1 = ( r11 - r22 - r33 + 1.0f ) / 4.0f;
+    double q2 = ( -r11 + r22 - r33 + 1.0f ) / 4.0f;
+    double q3 = ( -r11 - r22 + r33 + 1.0f ) / 4.0f;
+
+    if ( q0 < 0.0f )
+        q0 = 0.0f;
+    if ( q1 < 0.0f )
+        q1 = 0.0f;
+    if ( q2 < 0.0f )
+        q2 = 0.0f;
+    if ( q3 < 0.0f )
+        q3 = 0.0f;
+    q0 = sqrt( q0 );
+    q1 = sqrt( q1 );
+    q2 = sqrt( q2 );
+    q3 = sqrt( q3 );
+    if ( q0 >= q1 && q0 >= q2 && q0 >= q3 )
+    {
+        q0 *= +1.0f;
+        q1 *= SIGN( r32 - r23 );
+        q2 *= SIGN( r13 - r31 );
+        q3 *= SIGN( r21 - r12 );
+    }
+    else if ( q1 >= q0 && q1 >= q2 && q1 >= q3 )
+    {
+        q0 *= SIGN( r32 - r23 );
+        q1 *= +1.0f;
+        q2 *= SIGN( r21 + r12 );
+        q3 *= SIGN( r13 + r31 );
+    }
+    else if ( q2 >= q0 && q2 >= q1 && q2 >= q3 )
+    {
+        q0 *= SIGN( r13 - r31 );
+        q1 *= SIGN( r21 + r12 );
+        q2 *= +1.0f;
+        q3 *= SIGN( r32 + r23 );
+    }
+    else if ( q3 >= q0 && q3 >= q1 && q3 >= q2 )
+    {
+        q0 *= SIGN( r21 - r12 );
+        q1 *= SIGN( r31 + r13 );
+        q2 *= SIGN( r32 + r23 );
+        q3 *= +1.0f;
+    }
+    else
+    {
+        printf( "coding error\n" );
+    }
+    double r = NORM( q0, q1, q2, q3 );
+    q0 /= r;
+    q1 /= r;
+    q2 /= r;
+    q3 /= r;
+
+    return QQuaternion( q0, q1, q2, q3 );
+}
+
