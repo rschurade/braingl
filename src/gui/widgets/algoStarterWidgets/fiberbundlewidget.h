d5ffe95d7366adaf1d6c4220d0038e81dfac7bbd
schurade <rschurade@users.noreply.github.com>
2013-11-05 15:51:07 +0100

    first commit for fiber bundling

---
+++ src/gui/widgets/algoStarterWidgets/fiberbundlewidget.h
@@ -0,0 +1,51 @@
+/*
+ * fiberbundlewidget.h
+ *
+ * Created on: 04.11.2013
+ * @author Ralph Schurade
+ */
+
+#ifndef FIBERBUNDLEWIDGET_H_
+#define FIBERBUNDLEWIDGET_H_
+
+#include "../../../data/enums.h"
+
+#include <QtGui>
+
+class QProgressBar;
+class QPushButton;
+class Dataset;
+class Bundle;
+class SliderWithEditInt;
+
+class FiberBundleWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    FiberBundleWidget( Dataset* ds, QWidget* parent = 0 );
+    virtual ~FiberBundleWidget();
+
+    QList<Dataset*> getFibs();
+
+private:
+    QVBoxLayout* m_layout;
+
+    Bundle* m_bundler;
+
+    QPushButton* m_startButton;
+    QProgressBar* m_progressBar;
+    int m_progress;
+
+    SliderWithEditInt* m_iterations;
+
+private slots:
+    void start();
+    void slotProgress();
+    void slotFinished();
+
+signals:
+    void finished();
+};
+
+#endif /* FIBERBUNDLEWIDGET_H_ */
