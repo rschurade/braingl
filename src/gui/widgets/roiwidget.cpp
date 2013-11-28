fe12e4ceadafd08436804a0c133fa96c844330f0
schurade <rschurade@users.noreply.github.com>
2013-11-28 13:59:27 +0100

    removed unused buttons

--- src/gui/widgets/roiwidget.cpp
+++ src/gui/widgets/roiwidget.cpp
@@ -28,15 +28,15 @@ ROIWidget::ROIWidget( QWidget* parent ) :
     connect( m_treeView, SIGNAL( clicked( QModelIndex ) ), this, SLOT( treeClicked() ) );
 
     QHBoxLayout* buttonLayout = new QHBoxLayout();
-    m_newTopButton = new QPushButton( this );
-    m_newTopButton->setText( QString( "new top" ) );
-    m_newChildButton = new QPushButton( this );
-    m_newChildButton->setText( QString( "new child" ) );
+//    m_newTopButton = new QPushButton( this );
+//    m_newTopButton->setText( QString( "new top" ) );
+//    m_newChildButton = new QPushButton( this );
+//    m_newChildButton->setText( QString( "new child" ) );
     m_deleteButton = new QPushButton( this );
     m_deleteButton->setText( QString( "delete" ) );
 
-    buttonLayout->addWidget( m_newTopButton );
-    buttonLayout->addWidget( m_newChildButton );
+    //buttonLayout->addWidget( m_newTopButton );
+    //buttonLayout->addWidget( m_newChildButton );
     buttonLayout->addWidget( m_deleteButton );
 
 //    connect( m_upButton, SIGNAL( clicked() ), this, SLOT( moveItemUp() ) );
@@ -55,8 +55,8 @@ ROIWidget::ROIWidget( QWidget* parent ) :
     setFrameStyle( QFrame::StyledPanel | QFrame::Plain );
 
 
-    m_newTopButton->setEnabled( false );
-    m_newChildButton->setEnabled( false );
+//    m_newTopButton->setEnabled( false );
+//    m_newChildButton->setEnabled( false );
     m_deleteButton->setEnabled( false );
 
     setStyleSheet( "QPushButton { font:  bold 12px; max-height: 14px; margin-top: -1px } " );
