218f9ec7b1d5c78833ea87d4022686ff39ca06fb
Timm Wetzel <timm.wetzel@nf.mpg.de>
2013-12-08 00:47:46 +0100

    Cleanup visibility of Mac context creation struct; only set non-default depth.

--- src/gui/core_3_2_context.h
+++ src/gui/core_3_2_context.h
@@ -6,20 +6,40 @@
 #if defined(Q_OS_MAC)
 #if QT_VERSION <= 0x040805 && QT_VERSION >= 0x040800    // if less or equal to 4.8.5
 void* select_3_2_mac_visual(GDHandle handle, int depthBufferSize);
+
 #else
 #error __FILE__ __LINE__ This Qt version is not supported.
 #endif
 #endif
 
-struct core_3_2_context : public QGLContext {
-    core_3_2_context(const QGLFormat& format, QPaintDevice* device) : QGLContext(format,device) {}
-    core_3_2_context(const QGLFormat& format) : QGLContext(format) {}
-
+struct core_3_2_context : public QGLContext
+{
 #if defined(Q_OS_MAC)
 #if QT_VERSION <= 0x040805 && QT_VERSION >= 0x040800    // if less or equal to 4.8.5
-    virtual void* chooseMacVisual(GDHandle handle) {
+    QGLFormat core_3_2_format()
+    {
+        QGLFormat format = QGLFormat();
+        format.setVersion( 3, 2 );
+        format.setProfile( QGLFormat::CoreProfile );
+        format.setSampleBuffers( true );
+        return format;
+    }
+
+    core_3_2_context(const QGLFormat& format, QPaintDevice* device)
+        : QGLContext(core_3_2_format(), device)
+    {}
+    core_3_2_context(const QGLFormat& format)
+        : QGLContext(core_3_2_format())
+    {}
+
+    ~core_3_2_context()
+    {}
+
+    virtual void* chooseMacVisual(GDHandle handle)
+    {
         return select_3_2_mac_visual(handle, this->format().depthBufferSize());
     }
+
 #endif
 #endif
 };
