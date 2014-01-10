fd1fb114cfb2ec83d6dc11228098a683ea8c347b
schurade <schurade@localhost>
2014-01-10 09:58:56 +0100

    added VRML export for meshes

--- src/data/writer.h
+++ src/data/writer.h
@@ -10,6 +10,7 @@
 
 #include "../thirdparty/nifti/nifti1_io.h"
 
+#include <QFileInfo>
 #include <QString>
 
 class Dataset;
@@ -18,14 +19,14 @@ class TriangleMesh2;
 class Writer
 {
 public:
-    Writer( Dataset* dataset, QString fileName, QString file = "" );
+    Writer( Dataset* dataset, QFileInfo fileName, QString file = "" );
     virtual ~Writer();
 
     bool save();
 
 private:
     Dataset* m_dataset;
-    QString m_fileName;
+    QFileInfo m_fileName;
     QString m_filter;
 
     nifti_image* createHeader( int dim );
@@ -37,6 +38,7 @@ private:
     void saveROI();
     void saveConnexels();
     void saveOBJ();
+    void saveVRML();
 };
 
 #endif /* WRITER_H_ */
