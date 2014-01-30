dcd05b3f98905ff2e75abc2b3bced48438a16d96
joachim <joachim@visualistics.de>
2014-01-30 17:02:07 +0100

    no rendering of 0 arrays, because murg

--- src/gui/gl/diffpointglyphrenderer.cpp
+++ src/gui/gl/diffpointglyphrenderer.cpp
@@ -113,7 +113,7 @@ void DiffPointGlyphRenderer::draw( QMatrix4x4 p_matrix, QMatrix4x4 mv_matrix, in
     glShadeModel( GL_SMOOTH );
     glEnable( GL_POINT_SMOOTH );
 
-    if ( props->get( Fn::Property::D_DRAW_GLYPHS ).toBool() )
+    if ( props->get( Fn::Property::D_DRAW_GLYPHS ).toBool() && (np > 0) )
     {
         glDrawArrays( GL_POINTS, 0, np );
     }
