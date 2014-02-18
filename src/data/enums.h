184eb356cb69050bf5e957ed632aca52f01dd55a
schurade <schurade@localhost>
2014-02-18 13:25:57 +0100

    made tract color configurable

--- src/data/enums.h
+++ src/data/enums.h
@@ -338,6 +338,8 @@ namespace Fn
         G_MIN_COMPONENT_SIZE,
         G_RENDER_CROSSHAIRS_2,
         G_UNSELECTED_FIBERS_GREY,
+        G_TRACT_TEX_RESOLUTION,
+        G_TRACT_TEXT_SOURCE,
         G_LAST, // insert all global properties before this one
         // ROI Properties
         R_X = 1000,
@@ -594,6 +596,8 @@ namespace Fn
                 case Property::G_MIN_COMPONENT_SIZE: return QString( "min size for component extraction" ); break;
                 case Property::G_RENDER_CROSSHAIRS_2: return QString( "render crosshairs maingl2" ); break;
                 case Property::G_UNSELECTED_FIBERS_GREY: return QString( "render hidden fibers grey" ); break;
+                case Property::G_TRACT_TEX_RESOLUTION: return QString( "resolution of tract textures" ); break;
+                case Property::G_TRACT_TEXT_SOURCE: return QString( "source for tract textures" ); break;
                 case Property::G_LAST: return QString( "placeholder global last" ); break;
                 // ROI Properties
                 case Property::R_X: return QString( "x" ); break;
--- src/data/globalpropertymodel.cpp
+++ src/data/globalpropertymodel.cpp
@@ -105,6 +105,8 @@ GlobalPropertyModel::GlobalPropertyModel()
     m_properties->createInt( Fn::Property::G_FILTER_SIZE, 1, 1, 20, "algos" );
     m_properties->createInt( Fn::Property::G_MIN_COMPONENT_SIZE, 1000, 1, 100000, "algos" );
     m_properties->createFloat( Fn::Property::G_DECIMATE_EPSILON, 2.0f, 0.0f, 20.f, "algos" );
+    m_properties->createFloat( Fn::Property::G_TRACT_TEX_RESOLUTION, 1.0f, 0.2f, 5.f, "algos" );
+    m_properties->createList( Fn::Property::G_TRACT_TEXT_SOURCE, {"local", "global", "user defined"}, 0, "algos" );
 
     connect( m_properties->getProperty( Fn::Property::G_SCREENSHOT_WIDTH ), SIGNAL( valueChanged( QVariant ) ), this, SLOT( slotScreenShotWidth( QVariant ) ) );
     connect( m_properties->getProperty( Fn::Property::G_SCREENSHOT_HEIGHT ), SIGNAL( valueChanged( QVariant ) ), this, SLOT( slotScreenShotHeight( QVariant ) ) );
