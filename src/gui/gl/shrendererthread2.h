96b8ba5e5dd5dc1406bcabff882eb5be991266bc
schurade <rschurade@users.noreply.github.com>
2014-05-22 14:52:09 +0200

    wip refactor renderers

--- src/gui/gl/shrendererthread2.h
+++ src/gui/gl/shrendererthread2.h
@@ -8,6 +8,8 @@
 #ifndef SHRENDERERTHREAD2_H_
 #define SHRENDERERTHREAD2_H_
 
+#include "../../data/properties/propertygroup.h"
+
 #include "../../thirdparty/newmat10/newmat.h"
 
 #include <QDebug>
@@ -22,12 +24,11 @@ class SHRendererThread2 : public QThread
     Q_OBJECT
 
 public:
-    SHRendererThread2( int id, std::vector<ColumnVector>* data, int   m_nx,   int m_ny,   int m_nz,
-                                                     float m_dx, float m_dy, float m_dz,
-                                                     int   xi,     int yi,       int zi,
-                                                     int lod, int order, int orient, bool scaling, float scaleFactor,
-                                                     bool hideNegative,
-                                                     QMatrix4x4 pMatrix, QMatrix4x4 mvMatrix );
+    SHRendererThread2( int id,
+                         std::vector<ColumnVector>* data,
+                         QMatrix4x4 pMatrix,
+                         QMatrix4x4 mvMatrix,
+                         PropertyGroup& props );
     virtual ~SHRendererThread2();
 
     TriangleMesh2* getMesh();
@@ -39,28 +40,12 @@ private:
 
     std::vector<ColumnVector>* m_data;
 
-    int m_nx;
-    int m_ny;
-    int m_nz;
-    float m_dx;
-    float m_dy;
-    float m_dz;
-
-    int m_xi;
-    int m_yi;
-    int m_zi;
-
-    int m_lod;
-    int m_order;
-    int m_orient;
-    bool m_scaling;
-    float m_scaleFactor;
-    float m_hideNegative;
-
     QMatrix4x4 m_pMatrix;
     QMatrix4x4 m_mvMatrix;
 
     TriangleMesh2* m_mesh;
+
+    PropertyGroup& m_props;
 
 signals:
     void finished( TriangleMesh2* );
