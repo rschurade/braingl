0ecb641888b1fc66deb5005845519076d32f7c39
schurade <schurade@localhost>
2014-08-11 14:30:34 +0200

    made mri on fibers selectable

--- src/shaders/fiber.fs
+++ src/shaders/fiber.fs
@@ -8,6 +8,7 @@
 
 in float v_discard;
 uniform float u_fibGrowth;
+uniform int u_mriSource;
 
 void main()
 {
@@ -23,11 +24,40 @@ void main()
     }
     if ( u_colorMode == 3 )
     {
-        vec4 tcolor = texColor();
-	    
-	    if ( !( tcolor.r + tcolor.g + tcolor.b > 0.0 ) ) discard;
-	    
-	    color = tcolor.rgb;
+        vec4 tcolor = vec4( 0.0 );
+        vec4 color0 = vec4( 0.0, 0.0, 0.0, 0.0 );
+	    vec4 color1 = vec4( 0.0, 0.0, 0.0, 0.0 );
+	    vec4 color2 = vec4( 0.0, 0.0, 0.0, 0.0 );
+	    vec4 color3 = vec4( 0.0, 0.0, 0.0, 0.0 );
+	    vec4 color4 = vec4( 0.0, 0.0, 0.0, 0.0 );
+        if ( u_mriSource == 0 )
+        {
+            color0 = texture( texture0, v_texCoord0 );
+            tcolor = colormap( unpackFloat( color0 ), u_colormap, u_lowerThreshold, u_upperThreshold, u_selectedMin, u_selectedMax );
+        }
+        if ( u_mriSource == 1 )
+        {
+            color1 = texture( texture1, v_texCoord1 );
+            tcolor = colormap( unpackFloat( color1 ), u_colormap, u_lowerThreshold, u_upperThreshold, u_selectedMin, u_selectedMax );
+        }
+        if ( u_mriSource == 2 )
+        {
+            color2 = texture( texture2, v_texCoord2 );
+            tcolor = colormap( unpackFloat( color2 ), u_colormap, u_lowerThreshold, u_upperThreshold, u_selectedMin, u_selectedMax );
+        }
+        if ( u_mriSource == 3 )
+        {
+            color3 = texture( texture3, v_texCoord3 );
+            tcolor = colormap( unpackFloat( color3 ), u_colormap, u_lowerThreshold, u_upperThreshold, u_selectedMin, u_selectedMax );
+        }
+        if ( u_mriSource == 4 )
+        {
+            color4 = texture( texture4, v_texCoord4 );
+            tcolor = colormap( unpackFloat( color4 ), u_colormap, u_lowerThreshold, u_upperThreshold, u_selectedMin, u_selectedMax );
+        }
+        if ( !( tcolor.r + tcolor.g + tcolor.b > 0.0 ) ) discard;
+        
+        color = tcolor.rgb;
     }
     else if ( u_colorMode == 4 )
     {
